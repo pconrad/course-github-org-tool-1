7ef472cb23b4dd1ede6bc3c4dc353b0d926c4882
Nick Brown <nickbrown714@gmail.com>
2017-01-07 13:37:22 -0800

    updated and added model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,21 @@
 describe User do
 
-  before(:each) { @user = FactoryGirl.create(:user) }
+  before(:each) { @user = FactoryGirl.create(:user,
+    name: 'Test User',
+    username: 'testuser'
+  ) }
 
   subject { @user }
 
   it { should respond_to(:name) }
+  it { should respond_to(:username) }
 
   it "#name returns a string" do
     expect(@user.name).to match 'Test User'
+  end
+
+  it "#username returns a string" do
+    expect(@user.username).to match 'testuser'
   end
 
 end
